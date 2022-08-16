b256c70a2732116805016a4dab01881838101147
nurul90042 <wahdanurul.nw@gmail.com>
2022-08-17 05:04:47 +0700

    add some optional pict

--- resources/views/layouts/ixp-logo-header.foil.php
+++ resources/views/layouts/ixp-logo-header.foil.php
@@ -1,4 +1,4 @@
 <a class="navbar-brand" style="font-size:18px" href="<?= url('') ?>">
-    <img class="tw-inline" style="max-height: 26px !important; margin-right: 1rem !important;" src="<?= asset( "images/six/xxArtboard 1 copy 7@3x.png" ) ?>">
+    <img class="tw-inline" style="max-height: 50px !important; margin-right: 1rem !important;" src="<?= asset( "images/six/xxArtboard 1 copy 7@3x.png" ) ?>">
     <?= config('identity.titlename' ) ?>
 </a>
