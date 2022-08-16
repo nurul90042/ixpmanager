c114c1f8d230e8c06ff4598de2a2eaf055c337e9
nurul90042 <wahdanurul.nw@gmail.com>
2022-08-17 06:49:23 +0700

    improve-head-img

--- resources/views/layouts/ixp-logo-header.foil.php
+++ resources/views/layouts/ixp-logo-header.foil.php
@@ -1,4 +1,4 @@
 <a class="navbar-brand" style="font-size:18px" href="<?= url('') ?>">
-    <img class="tw-inline" style="max-height: 26px !important; margin-right: 1rem !important;" src="<?= asset( "images/six/xxArtboard 1 copy 8@3x.png" ) ?>">
+    <img class="tw-inline" style="max-height: 26px !important; margin-right: 1rem !important;" src="<?= asset( "images/six/header1.png" ) ?>">
     <?= config('identity.titlename' ) ?>
 </a>
